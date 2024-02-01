82ce187f13814fb6e171a8d8788b968bc9836849
Max <12960866+maxchapin@users.noreply.github.com>
2024-01-31 21:21:59 -0500

    fix pp

--- src/pages/PPolicy.jsx
+++ src/pages/PPolicy.jsx
@@ -6,9 +6,9 @@ function PrivacyPolicy() {
                 
             <div className="PPolicy">
 
-                <h1>Privacy Policy for<b> LLMCompare</b></h1>
+                <h1>Privacy Policy for LLMCompare</h1>
 
-                <p>At <b>LLMCompare</b>, accessible from <b>LLMCompare.com </b>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by <b>LLMCompare</b> and how we use it.</p>
+                <p>At <b>LLMCompare</b>, accessible from <b>LLMCompare.ai </b>, one of our main priorities is the privacy of our visitors. This Privacy Policy document contains types of information that is collected and recorded by <b>LLMCompare</b> and how we use it.</p>
 
                 <p>If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us at email <b>maxchapin430@gmail.com</b>.</p>
 
