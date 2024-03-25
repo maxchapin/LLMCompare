19e9c361d2b36ad52bc0cacb375b05d33e10aa24
Max <12960866+maxchapin@users.noreply.github.com>
2024-03-25 08:35:35 -0400

    add analytics

--- src/App.js
+++ src/App.js
@@ -9,21 +9,24 @@ import PrivacyPolicy from './pages/PPolicy';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ImageGeneration from './pages/ImageGeneration';
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    
-    <Router>  
-          <Header/>
-          <Routes>       
-              <Route exact path="/" Component={Home} />
-              <Route path="/Home" Component={Home} />
-              <Route path="/PrivacyPolicy" Component={PrivacyPolicy} />
-              <Route path="/ImageGeneration" Component={ImageGeneration} />
-              {/* Add more routes if you have other pages */}
-          </Routes>
-          <Footer/>
-      </Router>
+    <div>
+      <Router>  
+            <Header/>
+            <Routes>       
+                <Route exact path="/" Component={Home} />
+                <Route path="/Home" Component={Home} />
+                <Route path="/PrivacyPolicy" Component={PrivacyPolicy} />
+                <Route path="/ImageGeneration" Component={ImageGeneration} />
+                {/* Add more routes if you have other pages */}
+            </Routes>
+            <Footer/>
+        </Router>
+        <Analytics></Analytics>
+      </div>
   );
 }
 
